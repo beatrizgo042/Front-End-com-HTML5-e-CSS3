bedaa468251954e4d70a71f84c1bbe3eb018b307
Beatriz Gomes <beatrizgo042@gmail.com>
2021-11-20 13:16:49 -0300

    Acessibilidade web

--- JavaScript/js/form.js
+++ JavaScript/js/form.js
@@ -5,8 +5,6 @@ botaoAdicionar.addEventListener("click", function (event) {
     var formulario = document.querySelector(".form-adiciona");
     var paciente = obtemPacienteDoForm(formulario);
 
-    var pacienteTr = montaTr(paciente);
-
     var erros = validaPaciente(paciente);
     console.log(erros);
     
@@ -15,15 +13,19 @@ botaoAdicionar.addEventListener("click", function (event) {
         return;
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
+    adicionaPacienteNaTabela(paciente);
 
     formulario.reset();
 
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensDeErro (erros){
     var ul = document.querySelector("#mensagens-erro");
