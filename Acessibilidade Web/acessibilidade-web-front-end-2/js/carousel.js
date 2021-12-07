a062b9cd15b14e8054d20c8197f9f7881e4c1ae2
Beatriz Gomes <beatrizgo042@gmail.com>
2021-12-07 17:51:34 -0300

    Acessibilidade web parte 2

--- Acessibilidade Web/acessibilidade-web-front-end-2/js/carousel.js
+++ Acessibilidade Web/acessibilidade-web-front-end-2/js/carousel.js
@@ -1,9 +1,32 @@
-var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+var btns = document.querySelectorAll('.listaDeArtigos-slider-item'),
+    new0 = document.querySelector("#new0"),
+    new1 = document.querySelector("#new1"),
+    new2 = document.querySelector("#new2"),
+    noticias = document.querySelectorAll(".listaDeArtigos-item");
+
+
+    new0.style.display = "block";
+
+// Criando um indicador de slid atual
+var indicadorSlideAtual = document.createElement("span");
+indicadorSlideAtual.classList.add("escondeVisualmente");
+indicadorSlideAtual.id = "escondeVisualmente";
+indicadorSlideAtual.textContent = "(Slide atual)";
 
 // Percorre todos os botoes controladores
 btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
-    
+
+    noticias.forEach(function(noticia){
+      noticia.style.display = "none";
+
+      if(this.getAttribute("data-sliderItem") === noticia.getAttribute("data-noticia")) {
+        noticia.style.display = "block";
+      }
+    }.bind(this))
+
+    document.querySelector(".listaDeArtigos-slider-item .escondeVisualmente").remove();
+    this.append(indicadorSlideAtual);
 
     // Remove classe 'ativo' dos outros botoes
     btns.forEach(function(btnRemoveClass) {
--- Acessibilidade Web/acessibilidade-web-front-end-2/js/dialog.js
+++ Acessibilidade Web/acessibilidade-web-front-end-2/js/dialog.js
@@ -1,19 +1,40 @@
 // Variáveis
+var conteudoForaDialog = document.querySelector("#conteudoForaDialog");
+var controleVideo = document.querySelector(".secaoInstitucional-video");
+
 var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
+btnAbreDialog.style.display = "block";
+
+
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector(".dialogNewsletter-campo").focus();
+  conteudoForaDialog.inert = true;
+
+  controleVideo.revomeAttribute("controls");
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');
+  conteudoForaDialog.inert = false;
+  btnAbreDialog.focus();
+  controleVideo.setAttribute("controls", true);
+
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+dialogOverlay.addEventListener("click", fechandoDialog);
+
+document.addEventListener("keyup", function(evento) {
+  if(evento.keyCode == 27){
+    fechandoDialog();
+  }
+});
