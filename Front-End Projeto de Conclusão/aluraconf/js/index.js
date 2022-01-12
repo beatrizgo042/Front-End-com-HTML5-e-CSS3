aa68550263e006bb4110abc07ece7615a845eebb
Beatriz Gomes <beatrizgo042@gmail.com>
2022-01-12 15:39:12 -0300

    projeto de conclusão

---
+++ Front-End Projeto de Conclusão/aluraconf/js/index.js
@@ -0,0 +1,14 @@
+function ativaScrollSuave (selector){
+    $(selector).click(function(event){
+        event.preventDefault();
+        var target = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(target).offset().top
+        }, 500)
+    })
+}
+
+ativaScrollSuave("a[href*=painelSobre]");
+ativaScrollSuave("a[href*=painelPalestrantes]");
+ativaScrollSuave("a[href*=painelFormulario]");
